59c47a597bd290769991287a987f237765d8d101
Elliott Johnson <hello+git@ell.iott.dev>
2026-09-03 11:09:28 -0600

    final cleanup

--- src/uneval.js
+++ src/uneval.js
@@ -1,3 +1,5 @@
+/** @import { UnevalReplacer } from './types.js' */
+
 import { MAX_ARRAY_INDEX } from './constants.js';
 import {
 	DevalueError,
@@ -25,7 +27,7 @@ const reserved =
 /**
  * Turn a value into the JavaScript that creates an equivalent value
  * @param {any} value
- * @param {import('./types.js').UnevalReplacer} [replacer]
+ * @param {UnevalReplacer} [replacer]
  */
 export function uneval(value, replacer) {
 	const counts = new Map();
